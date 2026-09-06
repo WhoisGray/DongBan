c435a0a5a97d3ada5a977ce5c4f63bccac7df791
Yashar Razban <admin@graygroup.ir>
2026-09-07 03:31:59 +0800

    feat: integrate Vercel Analytics for tracking page views and update privacy policy in README

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { Analytics } from '@vercel/analytics/vue'
 import AppHeader from './components/AppHeader.vue'
 import ToastStack from './components/ToastStack.vue'
 import { useEventStore } from './stores/eventStore.js'
@@ -12,5 +13,6 @@ const store = useEventStore()
     <AppHeader />
     <main class="main-shell"><RouterView /></main>
     <ToastStack />
+    <Analytics />
   </div>
 </template>
